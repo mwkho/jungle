bccfa9df443c38495ff161b27064584605eda290
mwkho <martin.h@hotmail.ca>
2021-04-29 21:49:12 +0000

    Make user login work

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,10 +5,13 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-
-    if @user.save
-      session[:user_id] = @user.id
-      redirect_to :root
+    if @user.password == @user.password_confirmation
+      if @user.save
+        session[:user_id] = @user.id
+        redirect_to :root
+      else
+        render :new
+      end
     else
       render :new
     end
