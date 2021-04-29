3212c9d07dc82f0b01a553bcb4fcc6a19d1b4f8d
mwkho <martin.h@hotmail.ca>
2021-04-29 17:21:16 +0000

    Add creating new category functionality

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -4,10 +4,18 @@ class Admin::CategoriesController < ApplicationController
   end
 
   def new
-
+    @category = Category.new
   end
 
   def create
+    @category = Category.new(category_params)
 
+    if @category.save
+      redirect_to [:admin, :categories], notice: "'#{@category.name}' category has been created"
+      return
+    end
+
+    render :new
+    
   end
 end
