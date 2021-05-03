97b4f49e65955bd529541c0a98fd83708655c4bd
mwkho <martin.h@hotmail.ca>
2021-05-03 21:31:29 +0000

    Revert "Fix bugs in user-auth"
    This reverts commit 96147b329f2e0ac210b051416448bc327a92a47a.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,8 +6,7 @@ class SessionsController < ApplicationController
   def create 
     @user = User.find_by_email(params[:email])
     #  proceed to login if found user
-    if @user && @user.authenticate(params[:password])
-
+    if @user 
       session[:user_id] = @user.id
       redirect_to :root
     else
