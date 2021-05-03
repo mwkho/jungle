ec83eb3419fc81f0008d2d2e013fddab3c8de78a
mwkho <martin.h@hotmail.ca>
2021-05-03 21:56:23 +0000

    Add password validation in user model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,13 +5,9 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    if @user.password == @user.password_confirmation
-      if @user.save
-        session[:user_id] = @user.id
-        redirect_to :root
-      else
-        render :new
-      end
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to :root
     else
       render :new
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
 class User < ActiveRecord::Base
   has_secure_password
+
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :first_name, :last_name, :password, :password_confirmation, presence:true
+  validate :password_cannot_be_different_from_confirmation
+
+  def password_cannot_be_different_from_confirmation
+    if password != password_confirmation
+      errors.add(:password, "passwords need to match")
+    end
+  end
 end
