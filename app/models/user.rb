f8fdbf4fafad54ef93358d1e39a191e7d636211b
Your Name <your@email.com>
2021-05-05 06:13:54 +0000

    Update user error issues and format of login/logout"

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,15 +2,8 @@ class User < ActiveRecord::Base
   has_secure_password
 
   validates :email, presence: true, uniqueness: { case_sensitive: false }
-  validates :first_name, :last_name, :password, :password_confirmation, presence:true
+  validates :first_name, :last_name, :password, presence:true
   validates :password, length: { minimum: 3 }
-  validate :password_cannot_be_different_from_confirmation
-
-  def password_cannot_be_different_from_confirmation
-    if password != password_confirmation
-      errors.add(:password, "passwords need to match")
-    end
-  end
 
   def self.authenticate_with_credentials(email, password)  
     # find the user and proceed to authenticate
